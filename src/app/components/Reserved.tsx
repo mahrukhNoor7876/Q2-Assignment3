c0345db96fbe5582256bf8d0507496f00c256a97
Your Name <you@example.com>
2024-10-15 13:23:59 +0500

    Initial Commit

--- src/app/components/Reserved.tsx
+++ src/app/components/Reserved.tsx
@@ -1,6 +1,6 @@
 export default function Reserved(){
     return(
-        <div className="h-[35px] w-full bg-black flex items-center justify-center">
+        <div className="h-[35px] w-full bg-black flex items-center justify-center text-[13px] sm:text-[16px]">
             <p className="text-white">&copy; 2024, All rights reserved, Designed by Mahrukh Noor</p>
         </div>
     );
