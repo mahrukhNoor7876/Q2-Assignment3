4259c9a13d4f8bb3446976257115dd77cb38cb98
Your Name <you@example.com>
2024-10-15 12:59:39 +0500

    Initial Commit

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection(){
         <div>
             <header className="h-[470px] w-full grid grid-cols-1 lg:grid-cols-2 bg-gray-100">
                 <div className="pl-[50px] w-[91%]">
-                    <h1 className="text-blue-900 font-bold text-5xl mt-[3em] mb-[10px]">The Educators</h1>
+                    <h1 className="text-blue-900 font-bold text-5xl mt-[1.5em] sm:mt-[3em] mb-[10px]">The Educators</h1>
                     <p className="text-1xl text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi vel eaque possimus neque culpa quo, aut labore maxime tenetur doloribus recusandae! Laboriosam mollitia aperiam impedit unde earum fugit dignissimos sit?</p>
                     <p className="text-blue-900 font-bold cursor-pointer">Learn More</p>
                     <button className="text-white bg-blue-900 text-2xl p-[5px] pl-[10px] pr-[10px] rounded-md ml-auto mt-[10px] hover:bg-blue-800 hover:shadow-xl transition duration-200 active:scale-95">Register Now</button>
